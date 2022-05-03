ed8d93ea1b8842cfc233db16f626b390f2eec82a
minsgy <alstjrchl97@likelion.org>
2022-05-04 01:04:26 +0900

    🎨 style #2 : constants 파일 생성 및 셀 상태 플래그 정의 및 BOARD 크기 정의

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,9 @@
 export const CELL_SIZE = 42 as const;
 export const CELL_MARGIN = 2 as const;
 
+export const BOARD_SIZE = 8 as const;
+export const BOARD_MINE_COUNT = 6 as const;
+
 export const STATUS = {
   READY: 'ready',
   RUN: 'run',
@@ -9,11 +12,11 @@ export const STATUS = {
 };
 
 export const CELL_FLAG = {
-  MINE_QUESTION: -3,
-  MINE_FLAG: -2,
-  MINE: -1,
   OPEN: 0,
-  NOTHING: 1,
-  FLAG: 2,
-  QUESTION: 3,
+  NOTHING: -1,
+  FLAG: -2,
+  QUESTION: -3,
+  MINE: -4,
+  MINE_FLAG: -5,
+  MINE_QUESTION: -6,
 } as const;
