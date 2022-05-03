d7dcd4f2b8aca4718f31173b7c08ae850b40c324
minsgy <alstjrchl97@likelion.org>
2022-05-04 01:00:37 +0900

    ✨ feat #2 : 공통 버튼 컴포넌트 구현

---
+++ src/components/common/Button.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  cursor: pointer;
+  font-size: 16px;
+  font-weight: bold;
+  border: none;
+  background-color: transparent;
+`;
+
+export default StyledButton;
