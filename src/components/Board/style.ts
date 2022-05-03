b792665dd0a01183d8a4af5b0d228667e7368d69
minsgy <alstjrchl97@likelion.org>
2022-05-04 00:51:01 +0900

    ✨ feat #2:  Board Markup 및 Cell Event 구현

---
+++ src/components/Board/style.ts
@@ -0,0 +1,12 @@
+import { CELL_MARGIN, CELL_SIZE } from '@lib/constants';
+import styled from 'styled-components';
+
+const Wrapper = styled.div<{ boardSize: number }>`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin: 10px auto 0 auto;
+  width: ${({ boardSize }) => boardSize * (CELL_SIZE + CELL_MARGIN * 2)}px;
+`;
+
+export { Wrapper };
