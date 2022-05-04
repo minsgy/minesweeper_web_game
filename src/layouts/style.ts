03b7ecdf8d1fa7e0e8cd4bb5bf4bdacf863cf49d
minsgy <alstjrchl97@likelion.org>
2022-05-04 20:19:18 +0900

    ✨ feat #9 : 공통 템플릿 사용을 위한 Layout 정의

---
+++ src/layouts/style.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  width: 600px;
+  min-height: 100%;
+  margin: 0 auto;
+  padding: 0 16px;
+  background-image: linear-gradient(62deg, #fbab7e 0%, #f7ce68 100%);
+`;
+
+export { Wrapper };
