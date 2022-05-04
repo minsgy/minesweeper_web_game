e0811af23e789c04b9c68e9330e0f037b6d75fbe
minsgy <alstjrchl97@likelion.org>
2022-05-04 20:36:37 +0900

    ✨ feat #7 : 랭킹 페이지, 메인 페이지 정의 및 라우팅

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
-import MainPage from '../pages/MainPage';
+import MainPage from '~pages/MainPage';
+import RankPage from '~pages/RankPage';
 import { BrowserRouter, Routes as ReactRoutes, Route, Navigate } from 'react-router-dom';
 
 const Routes = () => {
@@ -6,6 +7,7 @@ const Routes = () => {
     <BrowserRouter>
       <ReactRoutes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/rank" element={<RankPage />} />
         <Route path="*" element={<Navigate to="/" />} />
       </ReactRoutes>
     </BrowserRouter>
